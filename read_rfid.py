4aa4affebbe645c41393f0215313ca65390abcfd
Ivan <iromero.py@gmail.com>
2022-03-07 18:22:59 +0100

    final puzzle1

--- read_rfid.py
+++ read_rfid.py
@@ -2,7 +2,6 @@ import csv
 import RPi.GPIO as GPIO
 import time
 
-from os import write
 from datetime import datetime
 from mfrc522 import SimpleMFRC522
 from pathlib import Path
@@ -10,13 +9,14 @@ from gpiozero import LED
 
 ID = "0X7BE9F095F7" # my UPC card uid
 
+RED_LED = LED(6)
+GREEN_LED = LED(5)
+
 path_csv = Path("loggs.csv")
 data = {
     "date": None,
     "ID": None,
-
 }
-
 
 class Rfid:
     def __init__(self):
@@ -35,12 +35,12 @@ class Rfid:
 
 def write_to_csv():
     first_write = not path_csv.is_file()
-
     with open(path_csv, "a") as f:
         field_names = data.keys()
         writer = csv.DictWriter(f, field_names)
         if first_write:
             writer.writeheader()
+
         writer.writerow(data)
 
 
@@ -50,32 +50,26 @@ def authentication(id):
 
 def main():
     rfid = Rfid()
-
-    red_led = LED(6)
-    green_led = LED(5)
-    red_led.on()
-    green_led.off()
-    
+    RED_LED.on()
+    GREEN_LED.off()
     try: 
         while True:
             uid = rfid.read_uid()
             if authentication(uid):
-
-                green_led.on()
-                red_led.off()
-
+                GREEN_LED.on()
+                RED_LED.off()
                 data["date"] = datetime.now()
                 data["ID"] = uid
                 write_to_csv()
                 print(f'Usuario autorizado con uid: { uid }')
 
             else:
-                red_led.blink(0.2,0.2)
+                RED_LED.blink(0.2,0.2)
                 print("Usuario no autorizado")
 
             time.sleep(2)
-            green_led.off()
-            red_led.on()
+            GREEN_LED.off()
+            RED_LED.on()
 
     finally:
         GPIO.cleanup()
